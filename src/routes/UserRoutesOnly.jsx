faf34275b06b917e0cc554dd47119e97b37efd2b
Yasin <yasinarafat2413@gmail.com>
2025-07-25 18:00:51 +0600

    user routes created to secure from admin or vendor

---
+++ src/routes/UserRoutesOnly.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import useAuth from '../hooks/useAuth';
+import useUserRole from '../hooks/useUserRole';
+import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner';
+import { Navigate } from 'react-router';
+
+const UserRoutesOnly = ({children}) => {
+    const { user, loading } = useAuth();
+    const { role, isPending } = useUserRole();
+    
+
+
+
+    if( loading || isPending ) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+
+    if(!user || role !== 'user') {
+        return <Navigate to="/forbidden"></Navigate>
+    }
+
+
+    return children;
+};
+
+export default UserRoutesOnly;
