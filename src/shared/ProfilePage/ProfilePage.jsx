10304d1b74d029393f11c3045dc52b560a5aa977
Yasin <yasinarafat2413@gmail.com>
2025-07-29 21:57:27 +0600

    useAuth added in profile page

--- src/shared/ProfilePage/ProfilePage.jsx
+++ src/shared/ProfilePage/ProfilePage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import useAuth from "../../hooks/useAuth";
 
-const ProfilePage = ({ user }) => {
+const ProfilePage = () => {
+    const { user } = useAuth();
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-blue-100 px-4">
             <div className="bg-white shadow-xl rounded-2xl p-10 w-full max-w-md text-center">
                 <img
                     className="w-32 h-32 rounded-full mx-auto object-cover border-4 border-blue-500"
-                    src={
-                        user?.photoURL
-                    }
+                    src={user?.photoURL}
                     alt="User Profile"
                 />
                 <h2 className="text-2xl font-bold mt-6 text-gray-800">
@@ -17,7 +18,6 @@ const ProfilePage = ({ user }) => {
                 <p className="text-gray-500 mt-2">
                     {user?.email || "user@example.com"}
                 </p>
-
             </div>
         </div>
     );
